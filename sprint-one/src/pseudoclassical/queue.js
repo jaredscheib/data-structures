f18ab6876b22e4772e816465a5c76671fe437a85
jaredscheib <jared.scheib@gmail.com>
2014-07-04 16:56:48 -0700

    Implement Queue class in pseudoclassical instantiation style

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this._storage = {};
+  this._top = 0;
+  this._bottom = 0;
 };
 
+Queue.prototype.enqueue = function(value){
+  this._storage[this._top] = value;
+  this._top++;
+  return this.size();
+};
 
+Queue.prototype.dequeue = function(){
+  if( this.size() < 1 ) return;
+  var result = this._storage[this._bottom];
+  delete this._storage[this._bottom];
+  this._bottom++;
+  return result;
+};
+
+Queue.prototype.size = function(){
+  return this._top - this._bottom;
+};
