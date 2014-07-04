35eb7338e4c1b5ec3752c6c89f3e4130290fb721
jaredscheib <jared.scheib@gmail.com>
2014-07-04 16:50:12 -0700

    Implement stack class in prototypal instantiation style

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,38 @@
 var makeStack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance._storage = {};
+  someInstance._size = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this._storage[this._size] = value;
+    this._size++;
+    return this.size();
+  },
+
+  pop: function(){
+    if( this.size() < 1 ) return;
+    this._size--;
+    var result = this._storage[this._size];
+    delete this._storage[this._size];
+    return result;
+  },
+
+  size: function(){
+    return this._size;
+  }
+};
 
 
+var myStack = makeStack();
+console.log(myStack.push(3));
+console.log(myStack.push(7));
+console.log(myStack.push(2));
+console.log(myStack.pop());
+console.log(myStack.push(9));
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
